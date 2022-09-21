ae50959d23fb872be55ebc4f41edd6f6bba5d4cc
A-fauzi <akhmadfauzi0101@gmail.com>
2022-09-21 21:12:46 +0700

    first commit

---
+++ database/migrations/2022_09_21_122338_create_user_detail_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_detail', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->enum('status_member', ['premium', 'normal']);
+            $table->string('favourites_destination')->nullable();
+            $table->bigInteger('wallet')->default(0)->unsigned();
+            $table->string('about');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_detail');
+    }
+};
